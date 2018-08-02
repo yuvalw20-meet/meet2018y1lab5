0067956ea100d61ec671072c592d07fbf18e572f
yuvalw20-meet <yuvalw20@meet.mit.edu>
2018-08-02 03:16:49 -0400

    Stopping here

---
+++ lab5_shell.py
@@ -0,0 +1,40 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> a = ['she','sells','sea','shells','by','the','sea','shore']
+>>> b='selfish shellfish'
+>>> c=[1,1,2,3,5,8,13]
+>>> len(c)
+7
+>>> c[:-2]
+[1, 1, 2, 3, 5]
+>>> b[3:4]
+'f'
+>>> a[3][:-1]
+'shell'
+>>> a[2:4]
+['sea', 'shells']
+>>> c[1] + c[2]
+3
+>>> 'by' in a
+True
+>>> 'self' in b
+True
+>>> a[2] == a[6]
+True
+>>> [1,2,5] in c
+False
+>>> 'sh' in c
+False
+>>> a[3] == b[8:13]
+False
+>>> dog = 'dalmatian'
+>>> len(dog)
+9
+>>> dogs = [dog,'poodle','boxer']
+>>> len(dogs)
+3
+>>> one = [1,2,3,4]
+>>> two = [7,6,5,4]
+>>> three = ["y1", "friends", "fun"]
+>>> 
